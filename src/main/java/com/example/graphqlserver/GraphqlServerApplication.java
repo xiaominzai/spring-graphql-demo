58fbc2a95f2d1363d95c17429c23c4ced44a9361
chenym <chenym@yingmi.cn>
2025-07-07 14:43:13 +0800

    [feat]修改代码格式

--- src/main/java/com/example/graphqlserver/GraphqlServerApplication.java
+++ src/main/java/com/example/graphqlserver/GraphqlServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GraphqlserverApplication {
+public class GraphqlServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GraphqlserverApplication.class, args);
+		SpringApplication.run(GraphqlServerApplication.class, args);
 	}
 
 }
